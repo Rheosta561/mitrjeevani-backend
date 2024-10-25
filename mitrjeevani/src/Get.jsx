d8b993b89de2922c71ad4e6753695858a53c25b7
Anubhav Mishra <manubhav731@gmail.com>
2024-10-25 16:16:15 +0530

    Round 1 , brainwave

--- mitrjeevani/src/Get.jsx
+++ mitrjeevani/src/Get.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Get() {
   return (
-    <div className='border h-10 w-32 rounded-full mx-auto text-center pt-2 bg-blue-50 border-zinc-900 shadow-md mt-4 hover:bg-blue-900 hover:text-zinc-50 transition-all'><a href="">Get Started</a></div>
+    <div className='border h-10 w-32 rounded-full mx-auto text-center pt-2 bg-blue-50 border-zinc-900 shadow-md mt-4 hover:bg-blue-900 hover:text-zinc-50 transition-all'><a href="/chat">Get Started</a></div>
   )
 }
 
