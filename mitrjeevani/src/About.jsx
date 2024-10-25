0f58f715d45c011d22fa060b24f982d6f22f9f49
Anubhav Mishra <manubhav731@gmail.com>
2024-10-25 15:58:49 +0530

    First Round

--- mitrjeevani/src/About.jsx
+++ mitrjeevani/src/About.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import Ayushi from './Ayushi'
+import Footer from './Footer'
 
 function About() {
   return (
     <div>
       <div className=''>
         <Ayushi/>
-        
+        <div className='border h-fit p-4 mt-14 w-2/3 mx-auto rounded-lg shadow-md bg-cyan-100 text- font-semibold'>Imagine an empathetic AI companion that truly understands you. This generative AI-powered chatbot detects minor to medium mental health concerns based on your words and facial expressions. It's not just a cold, analytical machine; it listens, comprehends, and provides personalized solutions tailored just for you. Picture it as a non-judgmental confidant, offering a safe space to pour your heart out, much like writing in a diary but with feedback and guidance. This platform aims to help you navigate your emotions and challenges, offering support that's both compassionate and intelligent, allowing you to achieve mental well-being without feeling judged or misunderstood. It's like having a caring friend who always knows exactly how to help.</div>
+
+
 
       </div>
+      <Footer/>
+
     </div>
   )
 }
